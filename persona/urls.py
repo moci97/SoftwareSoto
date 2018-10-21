98712013c3098305ac6201a76b00c8b15aef8c65
mariocond <2016237013@unfv.edu.pe>
2018-10-21 17:15:27 -0500

    html certificado listo

--- persona/urls.py
+++ persona/urls.py
@@ -1,9 +1,12 @@
-
 from django.urls import path
 from persona import views as persona_views
+from persona.views import *
+
 urlpatterns = [
     path('', persona_views.index),
     path('login/', persona_views.viewlogin),
     path('login/datos/', persona_views.validacion_login, name="login"),
-    path('login/datos/certificado/<int:id>', persona_views.certificado, name="certificado"),
+    path('login/datos/get_persona/', GetPersonas.as_view(), name="get_persona"),
+    path('login/datos/certificado/', Certificados.as_view(), name="get_certificado"),
+
 ]
--- persona/views.py
+++ persona/views.py
@@ -1,15 +1,19 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
+from django.views.generic import TemplateView
 
-from persona.forms import PersonaForm
 from persona.models import Persona
+
+
 # Create your views here.
 
 def index(request):
-    return render(request,'persona/index.html' )
+    return render(request, 'persona/index.html')
+
 
 def viewlogin(request):
-    return render(request,'persona/login.html')
+    return render(request, 'persona/login.html')
 
 
 def validacion_login(request):
@@ -18,20 +22,44 @@ def validacion_login(request):
         password = request.POST['password']
         user = authenticate(request, username=username, password=password)
         if user is not None:
-            login(request,user)
-            persona = Persona.objects.all()
-            contexto = {'persona': persona}
-            return render(request, 'persona/registro.html', contexto)
+            login(request, user)
+            return HttpResponseRedirect('/login/datos/get_persona/')
+
 
         else:
             return render(request, 'persona/login.html', {'error': 'Invalid username and password'})
     return render(request, 'persona/login.html')
 
-def certificado(request, id):
-    return render (request, 'persona/certificado.html')
+
+def certificado(request):
+    personas = request.session['personas_pk']
+    print("=====================")
+    print(str(personas))
+    print("=====================")
+    return render(request, 'persona/certificado.html')
 
 
+class Certificados(TemplateView):
+
+    template_name = "persona/certificado.html"
+
+    def get_context_data(self, **kwargs):
+        context = super(Certificados, self).get_context_data(**kwargs)
+        personas = Persona.objects.filter(pk__in=self.request.session['personas_pk']).values('nombre',
+                                                                                             'id_especialidad__id_facultad__nomb_uni')
+
+        context['persona'] = personas
+        return context
 
 
+class GetPersonas(TemplateView):
+    template_name = "persona/registro.html"
 
+    def get_context_data(self, **kwargs):
+        context = super(GetPersonas, self).get_context_data(**kwargs)
+        context['persona'] = Persona.objects.all()
+        return context
 
+    def post(self, request):
+        self.request.session['personas_pk'] = self.request.POST.getlist('persona_pk')
+        return HttpResponseRedirect('/login/datos/certificado/')
